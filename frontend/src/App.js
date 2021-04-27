311ccf5db865bcc71d552657fa082f8a8f476955
Fernando M <626melo@gmail.com>
2021-04-27 17:10:16 -0500

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,13 +25,6 @@ fetch("http://localhost:8000/api/products", {
     console.log(JSON.stringify(myJson));
   });
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
 function App() {
   const [sideToggle, setSideToggle] = useState(false);
 
